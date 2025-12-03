f02e22452dc9d3167e8fc811a114c01be2ac0f3b
Jemisse <macuacuajemisse@gmail.com>
2025-12-03 12:59:51 +0200

    feat: integra seções de projetos

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import JemisseCta from "./sections/CtaJemisse";
+import Unilib from "./sections/Projectos/UniLib";
+import Filmes from "./sections/Projectos/Filmes";
 
 export default function Home() {
   return (
     <div>
      <JemisseCta/>
+     <Unilib/>
+     <Filmes/>
       
     </div>
   );
