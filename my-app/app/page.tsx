44b5bcb7adb91da261fe2bd129c9c6a4d54b9c72
Jemisse <macuacuajemisse@gmail.com>
2025-12-03 14:13:17 +0200

    refactor: integra novas seções na página principal

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,23 @@
+
+
 import JemisseCta from "./sections/CtaJemisse";
 import Unilib from "./sections/Projectos/UniLib";
 import Jemisse from "./sections/Projectos/Jemisse";
+import Sobre from "./sections/Sobre";
+import Habilidades from "./sections/Habilidades";
+import ColorChangingFooter from "./sections/ColorChangingFooter";
+import { use } from "react";
 
 export default function Home() {
   return (
     <div>
-     <JemisseCta/>
-     <Unilib/>
-     <Jemisse/>
+      <JemisseCta />
+      <Unilib />
+      <Jemisse />
+      <Sobre />
+      <Habilidades />
+      <ColorChangingFooter />
+
     </div>
   );
 }
