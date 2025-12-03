d1d4ce9f772b966b75b199d5b06c3f5c669f3cd2
Jemisse <macuacuajemisse@gmail.com>
2025-12-03 12:19:46 +0200

    feat: adiciona seção principal (Hero)

---
+++ my-app/app/sections/CtaJemisse.tsx
@@ -0,0 +1,66 @@
+import EfeitoEscrever from "./efeitoEscrever";
+import NavbarCTA from "./navbarCTA";
+
+function JemisseCta() {
+  return (
+     
+    <div className="bg-gray-900">
+
+      <NavbarCTA/>
+
+
+      <div className="w-screen h-screen mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+         
+          <svg
+            viewBox="0 0 1024 1024"
+            aria-hidden="true"
+            className="absolute top-1/2 left-1/2 -z-10 size-256 -translate-y-1/2 mask-[radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+          >
+            <circle r={512} cx={200} cy={400} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+            <defs>
+              <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#7775D6" />
+                <stop offset={1} stopColor="#E935C1" />
+              </radialGradient>
+            </defs>
+          </svg>
+          <div className="mx-auto max-w-md lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+               <EfeitoEscrever />
+            <h2 className="text-4xl font-semibold tracking-tight text-balance text-white sm:text-4xl">
+              Jemisse Macuacua.
+            </h2>
+            <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-600 sm:text-4xl">
+              Apaixonado por tecnologia.
+            </h2>
+            <p className="mt-6 text-lg/8 text-pretty text-gray-400  font-mono">
+              Eu sou um Software Developer com mais de 1 ano de experiência em prover soluções para problemas usando tecnologia..
+            </p>
+            <div className="mt-10 flex   gap-x-6 lg:justify-start font-mono">
+              <a
+                href="mailto:macuacuajemisse@gmail.com"
+                className="rounded-md bg-sky-700 px-3.5 py-3.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                {' '}
+                Contactar-me{' '}
+              </a>
+              <a href="https://www.linkedin.com/in/jemisse-macu%C3%A1cua-5999b62bb/" className="rounded-md bg-gray-900 px-3.5 py-3.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+                LinkedIn
+                <span aria-hidden="true">→</span>
+
+              </a>
+            </div>
+          </div>
+          <br />
+          <br />
+          <br />
+
+
+        </div>
+      </div>
+    </div>
+ 
+  )
+}
+
+export default JemisseCta;
